fd5016298e26add7803589c6cdceb9a75a5719d5
vivek-dubey-0305 <vivek.dubey0305@gmail.com>
2025-12-03 09:41:26 +0530

    Fix multer upload directory creation

--- backend/middlewares/multer.middleware.js
+++ backend/middlewares/multer.middleware.js
@@ -1,11 +1,17 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
 
 //* local storage m rakhne k liye
 const storage = multer.diskStorage({
 
     destination: function (req, file, cb) {
         console.log("\n-----------\nMULTER.js\n-----------\n");
-        cb(null, "./public/temp/")
+        const uploadPath = "./public/temp/";
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+        cb(null, uploadPath)
 
 
     },
